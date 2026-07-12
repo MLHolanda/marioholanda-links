79e41c33b317c5c07b27512595fabafad8227763
MLHolanda <mariolucasholanda@gmail.com>
2026-07-12 15:44:10 -0300

    Refatora estrutura das ofertas e adiciona Drogasmil

---
+++ dados.js
@@ -0,0 +1,7 @@
+const oferta = {
+    nome: "Echo Dot 5ª Geração",
+    preco: "R$ 249,90",
+    precoAntigo: "R$ 399,90",
+    imagem: "img/echo-dot.jpg",
+    link: "https://www.amazon.com.br"
+};
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Projeto Mário Holanda iniciado!");
+
+document.getElementById("produto-imagem").src = oferta.imagem;
+
+document.getElementById("produto-nome").textContent = oferta.nome;
+
+document.getElementById("produto-antigo").textContent = oferta.precoAntigo;
+
+document.getElementById("produto-preco").textContent = oferta.preco;
+
+document.getElementById("produto-link").href = oferta.link;
