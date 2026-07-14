10c553ede47d6d4b8f620441aa6d5e6b7db60015
MLHolanda <mariolucasholanda@gmail.com>
2026-07-14 16:06:34 -0300

    Implementa rastreamento de cliques com Google Analytics

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let indiceAtual = 0;
+
+function registrarClique(indice) {
+
+    let cliques = JSON.parse(localStorage.getItem("cliques")) || {};
+
+    cliques[indice] = (cliques[indice] || 0) + 1;
+
+    localStorage.setItem("cliques", JSON.stringify(cliques));
+
+    console.log("Enviando evento:", ofertas[indice].nome);
+
+if (typeof gtag === "function") {
+    gtag("event", "clique_oferta", {
+        oferta: ofertas[indice].nome,
+        loja: ofertas[indice].nome
+    });
+}
+}
+   
 function mostrarOferta(indice) {
 
     const oferta = ofertas[indice];
@@ -10,13 +30,39 @@ function mostrarOferta(indice) {
 
     document.getElementById("produto-preco").textContent = oferta.preco;
 
-    document.getElementById("produto-link").href = oferta.link;
+    const botao = document.getElementById("produto-link");
+
+    botao.href = "#";
+
+    botao.onclick = function (e) {
+
+        e.preventDefault();
+
+        registrarClique(indice);
+
+        if (typeof gtag === "function") {
+
+            gtag("event", "clique_oferta", {
+                oferta: ofertas[indice].nome,
+                loja: ofertas[indice].nome,
+
+                event_callback: function () {
+                    window.open(oferta.link, "_blank");
+                }
+
+            });
+
+        } else {
+
+            window.open(oferta.link, "_blank");
+
+        }
+
+    };
 
 }
 
-// Mostra a primeira oferta
-mostrarOferta(0);
-let indiceAtual = 0;
+mostrarOferta(indiceAtual);
 
 setInterval(() => {
 
